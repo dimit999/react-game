498ac6b732d4bd3266892a335c7e42c8d539a45f
d.ananyev <Dzmitry.Ananyeu@digital14.com>
2021-02-26 21:50:44 +0400

    feat: modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].[hash].js',
+    // filename: '[name].[hash].js',
   },
   mode: 'development',
   devServer: {
