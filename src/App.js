1560a1a8b235a49ccbb4f95d845b9861a7189d45
d.ananyev <Dzmitry.Ananyeu@digital14.com>
2021-03-02 12:22:49 +0400

    feat: add start game pop upp

--- src/App.js
+++ src/App.js
@@ -6,27 +6,47 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       showComponent: false,
+      showBestResult: false,
     };
-    this._onButtonClick = this._onButtonClick.bind(this);
+    this._showGameComponent = this._showGameComponent.bind(this);
+    this._showBestResult = this._showBestResult.bind(this);
   }
 
-  _onButtonClick() {
+  _showGameComponent() {
     this.setState({
       showComponent: true,
+    });
+  }
+
+  _showBestResult() {
+    this.setState({
+      showBestResult: !this.state.showBestResult,
     });
   }
 
 render() {
   return (
     this.state.showComponent ?
-          <Game /> :
-          <div className='start-modal modal'>
-      <div className='start-modal-body modal-body'>
-        <div className="startGame" >
-        <button className="startGame" onClick={this._onButtonClick}>Start game</button>
-      </div>
-      </div>
-    </div>
-  )
+      <Game /> :
+        <div className='start-modal modal'>
+          <div className='start-modal-body'>
+            <div className="startGameContainer" >
+              <button className="startGame" onClick={this._showGameComponent}>Start game</button>
+              <button className="showBestResult" onClick={this._showBestResult}>Show best result</button>
+              {this.state.showBestResult && localStorage.getItem('snakeGameLength') != null && (
+                  <div className="result">
+                    <div className="bestResult">The best result is <strong>{localStorage.getItem('snakeGameLength')}</strong>.</div>
+                  </div>
+                )}
+              {this.state.showBestResult && localStorage.getItem('snakeGameLength') == null && (
+                <div className="result">
+                  <div className="bestResult">No any results yet.</div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )
+    }
 }
-}
+
